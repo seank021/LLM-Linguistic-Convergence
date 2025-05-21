2dd365cacf3aad942e2bb6147ac855112aba9b93
seank021 <seahnnnnnn@gmail.com>
2025-05-21 21:34:21 +0900

    fix: script

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -11,23 +11,27 @@ fi
 echo "Analyzing dialogues for conversation type: $CONV_TYPE"
 echo "" # line break
 
-echo "1. Analyzing readability..."
+echo "1. Analyzing Readability..."
 python3 analyze_readability.py $CONV_TYPE
 echo "" # line break
 
-echo "2. Analyzing parts of speech..."
+echo "2. Analyzing Parts of Speech..."
 python3 analyze_pos.py $CONV_TYPE
 echo "" # line break
 
-echo "3. Analyzing character-level features..."
+echo "3. Analyzing Character-Level Features..."
 python3 analyze_char.py $CONV_TYPE
 echo "" # line break
 
-echo "4. Analyzing word-level features..."
+echo "4. Analyzing Word-Level Features..."
 python3 analyze_word.py $CONV_TYPE
 echo "" # line break
 
-echo "5. Analyzing diversity..."
+echo "5. Analyzing LIWC..."
+python3 analyze_liwc.py $CONV_TYPE
+echo "" # line break
+
+echo "6. Analyzing Diversity..."
 python3 analyze_diversity.py $CONV_TYPE
 
 # echo "Analyzing vocabulary..."
